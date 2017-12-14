8ef0941c3027d436cee128bb5e0a051f2b246b69
macamarambio <marambio.macarena@gmail.com>
2017-12-14 09:36:10 -0300

    agregando desactivar y activar boton

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,42 @@
-$(document).ready(function() {
+$(document).ready( function(){
 /*splash*/
-	setTimeout(function() {
-		$(".content").fadeOut(500);
+	setTimeout( function() {
+		$('.content').fadeOut(500);
 		},3000);
-		setTimeout(function() {
-			$(".content2").fadeIn(500);
+		setTimeout( function() {
+			$('.content2').fadeIn(500);
 		});
 });
 
-/* Icon Select */
+
+/*Activar Boton*/
+$('#phone').keyup( function(){
+	if($(this).val().length === 10){
+		$('#boton').removeClass('disabled');
+	}if($(this).val().length < 10){
+		$('#boton').addClass('disabled');
+	}if($(this).val().length > 10){
+		$('#boton').addClass('disabled');
+		$('#phone').prop('disabled',true); /*Desactivar*/
+		$('#phone').prop('disabled',false); /*Activar*/
+	}if (isNaN($(this).val())){
+		$('#texto').text('Only Number');
+		$('#boton').addClass('disabled');
+	}else{
+		$('#texto').text('');
+	}
+	return false;
+});
+
+/*Enviar codigo*/
+
+function code() {
+  var code = "";
+  var str = "123456789";
+  for (var i = 0; i < 3; i++) {
+  	code += str.charAt(Math.floor(Math.random() * str.length));
+  }
+  return code;
+}
+
+alert(code());
